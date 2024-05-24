aa99dbc4b48853900b0fa302abcb6d871c8c91d6
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-24 06:29:53 +0000

    Fri, May 24, 2024, 11:59 AM +05:30

--- prac.py
+++ prac.py
@@ -1,6 +1,15 @@
-x = int(input("Enter value of x: "))
+def main():
+    x = int(input("Enter value of x: "))
+    if x % 2 == 0:
+        print("Even Number")
+    else:
+        print("Odd Number")
 
-if x % 2 == 0:
-    print("Even Number")
-else:
-    print("Odd Number")
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
