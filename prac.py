f9d61f6e584fe4a59f3a923aeb816dc94359c323
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-24 06:39:18 +0000

    Fri, May 24, 2024, 12:09 PM +05:30

--- prac.py
+++ prac.py
@@ -1,8 +1,9 @@
 name = input("Enter your name: ")
 
-if name == "Harry" or name == "Hermoine" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+match name:
+    case "Harry" | "Hermoine" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
