2e34ea76fe9af9d3d8b85a6018804d59b5f6644f
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 12:24:33 +0000

    Wed, May 29, 2024, 5:54 PM +05:30

--- prac.py
+++ prac.py
@@ -1,2 +1,9 @@
 def main():
-    x = int(input("))
+    x = int(input("Value of x: "))
+    print(f"Squared of {x} is {squared(x)}")
+
+def squared(n):
+    return n*n
+
+if __name__ == "__main__":
+    main()
