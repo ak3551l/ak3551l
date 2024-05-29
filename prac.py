2c92287c51c2f76bc8b90d61f0d7b5f5e909ef05
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 16:57:11 +0000

    Wed, May 29, 2024, 10:27 PM +05:30

--- prac.py
+++ prac.py
@@ -2,7 +2,6 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("Few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Many arguments")
 
-print("Hello, " + sys.argv[1])
+for arg in sys.argv:
+    print("Hello, " + arg)
