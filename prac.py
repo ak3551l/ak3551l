def7a398efb0fbf705158077ef836b482b6fb8ef
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 11:48:28 +0000

    Wed, May 29, 2024, 5:18 PM +05:30

--- prac.py
+++ prac.py
@@ -1,3 +1,15 @@
 def main():
-    name
-    hello()
+    name = input("Enter your name: ")
+    hello(name)
+    goodbye(name)
+
+
+
+def hello(input):
+    print("Hello, ", input)
+
+def goodbye(input):
+    print("Goodbye, ", input)
+
+
+main()
