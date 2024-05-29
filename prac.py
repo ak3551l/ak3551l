633709fbc1d0d197c31a55fc660a9765d5379df5
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 11:31:49 +0000

    Wed, May 29, 2024, 5:01 PM +05:30

--- prac.py
+++ prac.py
@@ -1,5 +1,8 @@
-import cowsay
 import sys
 
-if len(sys.argv) == 2:
-    cowsay.trex("Hello, " + sys.argv[1])
+if len(sys.argv) < 2:
+    print("Too few arguments.")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("my name is", sys.argv[1])
