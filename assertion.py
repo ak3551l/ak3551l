b93565abb3fb263b76cf4d8b597c01f8df7f066d
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 12:51:52 +0000

    Wed, May 29, 2024, 6:21 PM +05:30

--- assertion.py
+++ assertion.py
@@ -11,3 +11,20 @@ def test_squared():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+from calc import square
+
+def main():
+    test_squared()
+
+def test_squared():
+    assert square(2) == 4
+    assert square(3) == 9
+
+if __name__ == "__main__":
+    main()
