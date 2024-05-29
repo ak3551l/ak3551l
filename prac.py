703b55def15a49ff056bbc054c616629d734c3bd
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 12:55:49 +0000

    Wed, May 29, 2024, 6:25 PM +05:30

--- prac.py
+++ prac.py
@@ -4,14 +4,12 @@ def main():
     test_squared()
 
 def test_squared():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4.")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9.")
+
+    assert square(2) == 4
+    assert square(-2) == 4
+    assert square(3) == 9
+    assert square(2) == 4
+    assert square(2) == 4
 
 if __name__ == "__main__":
     main()
