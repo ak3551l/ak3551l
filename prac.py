b50d04a9af78423442111d8690570f8d4f4a7e60
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-05-29 12:57:39 +0000

    Wed, May 29, 2024, 6:27 PM +05:30

--- prac.py
+++ prac.py
@@ -8,8 +8,8 @@ def test_squared():
     assert square(2) == 4
     assert square(-2) == 4
     assert square(3) == 9
-    assert square(2) == 4
-    assert square(2) == 4
+    assert square(-3) == 4
+    assert square(0) == 0
 
 if __name__ == "__main__":
     main()
