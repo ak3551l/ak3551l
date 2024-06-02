154434d15d5d9cd69747ac2d3d79d04586c24245
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-02 05:50:54 +0000

    Sun, Jun 2, 2024, 11:20 AM +05:30

--- prac.py
+++ prac.py
@@ -1,10 +1,4 @@
-class Point():
-
-    def __init__(self, input1, input2):
-        self.x = input1
-        self.y = input2
-
-p = Point(2, 8)
-
-print(p.x)
-print(p.y)
+count = 0
+while count > 3:
+    print("meow")
+    count += 1
