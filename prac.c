8d3df0d63d6c6f9027ff95883eab8563c4bc7f36
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-04 16:01:59 +0000

    Tue, Jun 4, 2024, 9:31 PM +05:30

--- prac.c
+++ prac.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <string.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    string name = get_string("Name: ");
-    int length = strlen(name);
-    printf("%i\n", length);
+    if (argc != 2)
+    {
+        printf("Missing command line argument\n");
+        return 1;
+    }
+    printf("Hello, %s\n", argv[1]);
+    return 0;
 }
