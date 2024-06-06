c288f504cee5590f04e074fd063e29edf6c8db5f
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-06 17:21:16 +0000

    Thu, Jun 6, 2024, 10:51 PM +05:30

---
+++ test/deep.py
@@ -0,0 +1,6 @@
+question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if (question == "42" or question == "forty-two" or question == "forty two" or question == "FoRty TwO" or question == "42 "  or question == " 42"):
+    print("Yes")
+else:
+    print("No")
+
