93369fe732ec4d20c8a6859aabe388571d7d403b
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-11 05:26:50 +0000

    Tue, Jun 11, 2024, 10:56 AM +05:30

--- prac.py
+++ prac.py
@@ -1,28 +1,30 @@
 class Student():
-    def __init__(self, name, house):
+    def __init__(self, first, last, house):
 
-        if not name:
+        if not :
             raise ValueError("Missing Name")
 
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid House")
 
-        self.name = name
+        self.first = first
+        self.last = last
         self.house = house
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(f"{student.first} {student.last} from {student.house}")
 
 
 
 def get_student():
 
 
-    name = input("Name: ")
+    first = input("First: ")
+    last = input("Last: ")
     house = input("House: ")
-    student = Student(name, house)
+    student = Student(first, last, house)
     return student
 
 if __name__ == "__main__":
