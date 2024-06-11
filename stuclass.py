347fd0cd4a7e7786c06c37562714f11322142a41
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-11 15:59:34 +0000

    Tue, Jun 11, 2024, 9:29 PM +05:30

--- stuclass.py
+++ stuclass.py
@@ -4,4 +4,19 @@ class Student():
         self.name = name
         self.house = house
 
-    
+
+def main():
+    student = get_student()
+    print(f"{self.name} from {}")
+
+
+def get_student():
+
+    name = input("Name: ")
+    house = input("House: ")
+
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
