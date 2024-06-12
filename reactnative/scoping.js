2240916029274270dc4a15df8557a1ea5e4a180c
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-12 12:53:32 +0000

    Wed, Jun 12, 2024, 6:23 PM +05:30

--- reactnative/scoping.js
+++ reactnative/scoping.js
@@ -1,4 +1,5 @@
-console.log(thisIsAConst)
+thisIsNotHoisted()
+
 const thisIsAConst = 50
 // thisIsAConst++ // error!
 
@@ -8,5 +9,18 @@ constObj.a = 'a'
 let thisIsALet = 51
 thisIsALet = 50
 
-let thisIsALet = 51 // errors!
+// let thisIsALet = 51 // errors!
 
+var thisIsAVar = 50
+thisIsAVar = 51
+var thisIsAVar = 'new value!'
+
+//function can be changed
+function thisIsHoisted() {
+    console.log("This is the function hoisted at the bottom of a file");
+}
+
+// const can't be changed
+const thisIsNotHoisted = function() {
+    console.log("Should not be Hoisted?")
+}
