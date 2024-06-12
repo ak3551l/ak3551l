5b012e5c225381ac9a57ac72df64361e72d5f763
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-12 12:23:50 +0000

    Wed, Jun 12, 2024, 5:53 PM +05:30

---
+++ reactnative/scoping.js
@@ -0,0 +1,11 @@
+console.log(thisIsAConst)
+const thisIsAConst = 50
+// thisIsAConst++ // error!
+
+const constObj = {}
+constObj.a = 'a'
+
+let thisIsALet = 51
+thisIsALet = 50
+
+let thisIsALet = 51 // errors!
