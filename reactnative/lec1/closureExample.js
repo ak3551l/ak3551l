eb37ae71b0dd982c2b5faadfc4947e267d515670
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-13 17:53:33 +0000

    Thu, Jun 13, 2024, 11:23 PM +05:30

--- reactnative/lec1/closureExample.js
+++ reactnative/lec1/closureExample.js
@@ -9,6 +9,7 @@ function  makeHelloFunction() {
 
 const sayHello = makeHelloFunction()
 
-// console.log(message)
+console.log('typeof message: ', typeof message)
+console.log(sayHello.toString())
 
 sayHello()
