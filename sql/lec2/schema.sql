4a58faa7fe0c410827875fbbf5e6c2358ca4484b
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-19 05:52:53 +0000

    Wed, Jun 19, 2024, 11:22 AM +05:30

--- sql/lec2/schema.sql
+++ sql/lec2/schema.sql
@@ -14,8 +14,10 @@ CREATE TABLE "stations" (
 
 
 CREATE TABLE "visits" (
+    "id" INTEGER,
     "rider_id" INTEGER,
     "station_id" INTEGER,
+    PRIMARY KEY("id"),
     FOREIGN KEY("rider_id") REFERENCES "riders"("id"),
     FOREIGN KEY("station_id") REFERENCES "stations"("id")
 );
