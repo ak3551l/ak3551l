819ab1b15d82157616514db22052c12a5c20dc6b
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 17:26:57 +0000

    Sun, Jun 23, 2024, 10:56 PM +05:30

--- prac.py
+++ prac.py
@@ -3,9 +3,12 @@ students = []
 with open("students.csv", "r") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}.")
+        student = {}
+        student['name'] = name
+        student['house'] = house
+        students.append(student)
 
-for student in sorted(students):
-    print(student)
+for student in (students):
+    print(f"{student['name']} is in {student['house']}")
 
 print(students)
