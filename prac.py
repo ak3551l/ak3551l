45f01e5c33b4c267bed874bf90cad2f79ab25050
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 10:51:24 +0000

    Sun, Jun 23, 2024, 4:21 PM +05:30

--- prac.py
+++ prac.py
@@ -2,4 +2,12 @@ from hello import hello
 
 def test_hello():
     assert hello("David") == "hello, David":
-    
+
+
+def test_default():
+    assert hello() == "hello, world"
+
+def test_arguments():
+
+    for name in ["Harry", "Hermoine", "Ron"]:
+        assert 
