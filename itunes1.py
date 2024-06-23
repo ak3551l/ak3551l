83f95d4285bbdf9ff2de303d00b27f58ac673732
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 10:45:19 +0000

    Sun, Jun 23, 2024, 4:15 PM +05:30

--- itunes1.py
+++ itunes1.py
@@ -7,4 +7,7 @@ if len(sys.argv) != 2:
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
 
-print(json.dumps(response.json(), indent=3))
+o = response.json()
+
+for result in o["results"]:
+    print(result["trackName"])
--- prac.py
+++ prac.py
@@ -1,10 +1,1 @@
-import json
-import sys
-import requests
-
-if len(sys.argv) != 2:
-    sys.exit()
-
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-
-print(response.json())
+from hello import hello
