fb0baae98db637185aad86629de9f05eca771fd2
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 17:34:02 +0000

    Sun, Jun 23, 2024, 11:04 PM +05:30

--- prac.py
+++ prac.py
@@ -3,12 +3,10 @@ students = []
 with open("students.csv", "r") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {}
-        student['name'] = name
-        student['house'] = house
+        student = {'name': name, 'house': house}
         students.append(student)
 
 for student in (students):
     print(f"{student['name']} is in {student['house']}")
 
-print(students)
+
