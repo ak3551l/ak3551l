57ad020c7c14a93e4680ca33e2eff9787d23cf8a
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 12:20:51 +0000

    Sun, Jun 23, 2024, 5:50 PM +05:30

--- prac.py
+++ prac.py
@@ -1,4 +1,11 @@
+names = []
+
 with open("names.txt", "r") as file:
 
-    for line in sorted(file):
-        print("Hello, ", line.rstrip())
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print("Hello, ", name)
+
+print(names)
