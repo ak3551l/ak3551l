f632c3de8e594d1e1598885f0908de907cc43846
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-23 07:11:21 +0000

    Sun, Jun 23, 2024, 12:41 PM +05:30

--- prac.py
+++ prac.py
@@ -1,5 +1,11 @@
-import cowsay
+import json
 import sys
+import requests
 
-if len(sys.argv) == 2:
-    cowsay.trex("Hello," + sys.argv[1])
+if len(sys.argv) != 2:
+    sys.exit()
+
+response =
+requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+print(response.json)
