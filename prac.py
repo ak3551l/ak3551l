9f0f705e01d094eef2c49a1cf928e0ccd648f10d
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-25 06:23:55 +0000

    Tue, Jun 25, 2024, 11:53 AM +05:30

--- prac.py
+++ prac.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email?: ").rstrip()
 
-if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.edu$", email):
+if re.search(r"\w+@\w+\.edu$", email):
     print("Valid")
 
 else:
