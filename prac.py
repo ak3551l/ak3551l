66c9102a6e80c060e16e58f85f3217b84279a8ff
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-25 06:37:30 +0000

    Tue, Jun 25, 2024, 12:07 PM +05:30

--- prac.py
+++ prac.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email?: ").rstrip()
 
-if re.search(r"\w+@\w+\.(com|edu|gov|net|gmail)$", email):
+if re.search(r"\w+@\w+\.(com|edu|gov|net|gmail)$", email, re.IGNORECASE):
     print("Valid")
 
 else:
