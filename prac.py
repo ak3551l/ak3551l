d60284b8ca293d4b453c6688bcda24daf3fa3837
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-25 05:21:31 +0000

    Tue, Jun 25, 2024, 10:51 AM +05:30

--- prac.py
+++ prac.py
@@ -1,9 +1,11 @@
+import csv
+
 students = []
+
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {'name': name, 'house': house}
-        students.append(student)
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
