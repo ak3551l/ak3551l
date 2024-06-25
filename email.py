91499f5f81e10d94565e78e1002ff2a6856aacb6
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-25 07:00:09 +0000

    Tue, Jun 25, 2024, 12:30 PM +05:30

--- email.py
+++ email.py
@@ -2,8 +2,9 @@ import re
 
 email = input("What's your email? ")
 
-if re.search(r"\w+@\w+\. \w+\.edu$", email, re.IGNORECASE):
+if re.search(r"\w+@\w+\.(com|edu|gov|net|gmail)$", email, re.IGNORECASE):
     print("Valid")
 
 else:
     print("Invalid")
+
