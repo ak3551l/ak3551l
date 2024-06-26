38c5bfeef70fe45b82097111fda5839113a9b727
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-26 09:55:31 +0000

    Wed, Jun 26, 2024, 3:25 PM +05:30

--- prac.c
+++ prac.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (int argc, string argv) {
 
-    printf("My name is %s\n", argv[1]);
+int main (int argc, string argv[]) {
+
+    printf("Hello %s\n", argv[1]);
 }
