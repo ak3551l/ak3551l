29da59220215c72528e7a537f5825fbaf7c36c8c
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-26 11:30:37 +0000

    Wed, Jun 26, 2024, 5:00 PM +05:30

--- prac.c
+++ prac.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
-#include <cs50.h>
-#include <string.h>
 
-int main(void) {
-
-    string phrase = get_string("Phrase: ");
-    int length = strlen(phrase);
-
-    for (int i = 0; i < length; i++)
-    {
-        if (phrase[i] > phrase[i + 1])
-        {
-            printf("Not in alphabetical order\n");
-            return 0;
-        }
-        else {
-            printf("In alphabetical order\n");
-            return 0;
-        }
-    }
+int main() {
+  int a = 5;
+  
+  printf("%d", !(x > 5 && x < 10));
+  return 0;
 }
-
