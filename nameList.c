ea9b499f777645314f4da030e35ab762ca2e234a
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-26 10:40:33 +0000

    Wed, Jun 26, 2024, 4:10 PM +05:30

--- nameList.c
+++ nameList.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <string.h>
 
 int main(void) {
+
+    int students[4];
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d\n", students[i]);
+    }
     
 }
