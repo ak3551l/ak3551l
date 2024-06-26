4735ebd38eb0b671d8f8526b6fd730b1c71e80a0
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-26 09:57:08 +0000

    Wed, Jun 26, 2024, 3:27 PM +05:30

--- prac.c
+++ prac.c
@@ -4,5 +4,11 @@
 
 int main (int argc, string argv[]) {
 
-    printf("Hello %s\n", argv[1]);
+    if (argc == 2) {
+        printf("Hello, %s\n", argv[1]);
+    }
+
+    else{
+        printf("Hello, World\n");
+    }
 }
