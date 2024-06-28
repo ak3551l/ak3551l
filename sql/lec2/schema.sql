b3f7c71b0772731f5055688699e33c98e3d525a6
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-28 01:19:07 +0000

    Fri, Jun 28, 2024, 6:49 AM +05:30

--- sql/lec2/schema.sql
+++ sql/lec2/schema.sql
@@ -1,27 +1,19 @@
-CREATE TABLE "cards" (
+CREATE TABLE riders (
     "id" INTEGER,
     "name" TEXT,
     PRIMARY KEY("id")
 );
 
-CREATE TABLE "stations" (
+CREATE TABLE stations (
     "id" INTEGER,
     "name" TEXT NOT NULL UNIQUE,
     "line" TEXT NOT NULL,
     PRIMARY KEY("id")
-
 );
 
-
-CREATE TABLE "swipes" (
-    "id" INTEGER,
-    "card_id" INTEGER,
-    "station_id" INTEGER,
-    "type" TEXT NOT NULL CHECK("type" IN ('enter', 'exit', 'deposit')),
-    "datetime" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "amout" NUMERIC NOT NULL CHECK("amount" != 0),
-    PRIMARY KEY("id"),
-    FOREIGN KEY("card_id") REFERENCES "cards"("id"),
+CREATE TABLE visits (
+    "rider_id" INTEGER,
+    "station_id" INTEGER
+    FOREIGN KEY("rider_id") REFERENCES "riders"("id"),
     FOREIGN KEY("station_id") REFERENCES "stations"("id")
 );
-
