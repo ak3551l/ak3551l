9ba4348febcdbfe706d2aa9244bc12cf523bf313
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-06-28 10:33:41 +0000

    Fri, Jun 28, 2024, 4:03 PM +05:30

--- sql/lec3/schema.sql
+++ sql/lec3/schema.sql
@@ -1,33 +1,7 @@
-CREATE TABLE "collections" (
+CREATE TABLE collections (
     "id" INTEGER,
-    "title" TEXT  NOT NULL,
+    "title" TEXT NOT NULL,
     "accession_number" TEXT NOT NULL UNIQUE,
-    "acquire" NUMERIC,
+    "acquired" NUMERIC,
     PRIMARY KEY("id")
 );
-CREATE TABLE "artists" (
-    "id" INTEGER,
-    "name" TEXT NOT NULL,
-    PRIMARY KEY("id")
-);
-CREATE TABLE "created" (
-    "artist_id" INTEGER,
-    "collection_id" INTEGER,
-    PRIMARY KEY("artist_id", "collection_id"),
-    FOREIGN KEY("artist_id") REFERENCES "artists"("id") ON DELETE CASCADE,
-    FOREIGN KEY("collection_id") REFERENCES "collections"("id") ON DELETE CASCADE
-);
-
-CREATE TABLE "transactions" (
-    "id" INTEGER,
-    "title" TEXT,
-    "action" TEXT,
-    PRIMARY KEY("id")
-);
-CREATE TRIGGER "sell"
-BEFORE DELETE ON "collections"
-FOR EACH ROW
-BEGIN
-    INSERT INTO "transactions" ("title", "action")
-    VALUES (OLD."title", 'sold');
-END;
