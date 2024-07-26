a7e49d5cfaf75836ee0b7a4d10979edafd4df9a1
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-07-26 15:39:18 +0000

    Fri, Jul 26, 2024, 9:09 PM +05:30

--- prac.c
+++ prac.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int a = 8;
+    int b = 56;
+    int sum = a + b;
+    printf("%i\n", sum);
+
 }
 
-int a = 8;
-int b = 56;
-int sum
-=
- a
-+
-b;
+
+
 printf("
 %i
 ", sum
