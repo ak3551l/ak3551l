1c9b3a4c5aff3876da204cb5beb7abd07741939b
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-08-12 02:36:10 +0000

    Mon, Aug 12, 2024, 8:06 AM +05:30

---
+++ java/OOPS.java
@@ -0,0 +1,8 @@
+import bank;
+
+class OOPS {
+    public static void main(String[] args) {
+        bank.Account account1 = new bank.Account();
+        account1.name = "customer1";
+    }
+}
--- java/bank/Bank.java
+++ java/bank/Bank.java
@@ -1,3 +1,29 @@
+package bank;
+
+class Account  {
+    public String name;
+    protected String email;
+    private String password;
+
+    // getters & setters
+    public String getPassword() {
+        return this.password;
+    }
+
+    // setters
+    public void setPassword(String pass) {
+        this.password = pass;
+    }
+
+}
 public class Bank {
-    
+    //we make main class public for accessible
+    public static void main(String args[]) {
+        Account account1 = new Account();
+        account1.name = "Apna College";
+        account1.email = "apnacollege@gmail.com";
+        account1.setPassword("abcd");
+        System.out.println(account1.getPassword());
+
+    }
 }
