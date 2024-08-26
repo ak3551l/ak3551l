0fb87567666c55c43997c78446d76a412862d533
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-08-26 16:01:15 +0000

    Mon, Aug 26, 2024, 9:31 PM +05:30

--- prac.c
+++ prac.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int add(int a, int b);
+
 int main(void) {
     int x = get_int("X: \n");
     int y = get_int("Y: \n");
 
-    printf("Add: %i\n", x+y);
+    printf("Add: %i\n", add(x, y));
+}
+
+int add(int a, int b) {
+    int c = a + b;
+    return c;
 }
 
 
