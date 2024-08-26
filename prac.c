dfafd197584a2e7f20fc5bc9c38da75d22e9e2f1
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-08-26 15:58:49 +0000

    Mon, Aug 26, 2024, 9:28 PM +05:30

--- prac.c
+++ prac.c
@@ -5,13 +5,7 @@ int main(void) {
     int x = get_int("X: \n");
     int y = get_int("Y: \n");
 
-    if (x > y) {
-        printf("x is greater than y.\n");
-    }
-
-    elif (x < y) {
-        printf("x is less than y.\n");
-    }
+    printf("Add: %i\n", x+y);
 }
 
 
