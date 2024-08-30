927ccaea58d64248ced039f8e72dcb47f518fe0c
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-08-30 17:08:15 +0000

    Fri, Aug 30, 2024, 10:38 PM +05:30

--- prac.c
+++ prac.c
@@ -4,7 +4,11 @@
 int main (void) {
     char c = get_char("Do you agree? ");
 
-    if (c  == 'Y' || 'y') {
-        printf(")
+    if (c  == 'Y' || c == 'y') {
+        printf("Agreed.\n");
+    }
+
+    if (c == 'N' || c == 'n') {
+        printf("Not Agreed.\n");
     }
 }
