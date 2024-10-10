a3a3259d06d823faee5d089d91bff58918a773b3
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-10-10 15:54:38 +0000

    Thu, Oct 10, 2024, 9:24 PM +05:30

--- python/maze.py
+++ python/maze.py
@@ -1,53 +1,16 @@
-import sys
+function spinWords(string){
+  //TODO Have fun :)
+  return string.split(' ') // Split the string into words
+  .map(word => {
+    // Reverse the word if its length is greater than 4, else leave it as it is
+      return word.length > 4 ? word.split('').reverse().join('') : word;
+  })
+  .join(' '); // Join the words back into a sentence
 
-class Node():
-    def __init__(self, state, parent, action):
-        self.state = state
-        self.parent = parent
-        self.action = action
-
-class StackFrontier():
-    def __init__(self):
-        self.frontier  = []
-
-    def add(self, node):
-        self.frontier.append(node)
-
-    def contains_state(self, state):
-        return any(node.state == state for node in self.frontier)
-
-    def empty(self):
-        return len(self.frontier) == 0
-
-    def remove(self):
-        if self.empty():
-            raise Exception("empty frontier")
-        else:
-            node = self.frontier[-1]
-            self.frontier = self.frontier[:-1]
-            return node
-
-class QueueFrontier(StackFrontier):
-    def remove(self):
-        if self.empty():
-            raise Exception("empty frontier")
-        else:
-            node = self.frontier[0]
-            self.frontier = self.frontier[1:]
-            return node
-
-class Maze();
-    def __init__(self, filename):
-        # Read file and set height and width of maze
-        with open(filename) as f:
-            contents = f.read()
-
-        # Validate start and goal
-        if contents.count("A") != 1:
-            raise Exception("maze must have exactly one start point")
-
-        if contents.count("B") != 1
-        raise Exception("maze must have exactly one goal")
+}
 
 
-
+// Test cases
+console.log(spinWords("Hey fellow warriors"));  // Output: "Hey wollef sroirraw"
+console.log(spinWords("This is a test"));       // Output: "This is a test"
+console.log(spinWords("This is another test")); // Output: "This is rehtona test"
