0d09c3b0e731054a8dca07993a98fab8c96e8bce
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-10-15 15:13:48 +0000

    Tue, Oct 15, 2024, 8:43 PM +05:30

--- html/prac.js
+++ html/prac.js
@@ -2,7 +2,7 @@ const key = {
     fullname: {
         'first_name': 'Abhijeet',
         'last_name': 'Kumar'
-    };
+    }
 }
 
-console.log(key['Fullname']['first_name']);
+console.log(key['fullname']['first_name']);
