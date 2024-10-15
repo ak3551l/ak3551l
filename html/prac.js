023329e0ee241bc35a801c4945ea2686de5995fb
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-10-15 01:46:46 +0000

    Tue, Oct 15, 2024, 7:16 AM +05:30

--- html/prac.js
+++ html/prac.js
@@ -1,12 +1,2 @@
-let evenNumbers = [];
-let oddNumbers = [];
-for (let i=0; i<=20; i++) {
-    if (i % 2 === 0) {
-        evenNumbers.push(i);
-    } else {
-        oddNumbers.push(i);
-    }
-}
-
-console.log(evenNumbers);
-console.log(oddNumbers);
+const num = 1 === "1";
+console.log(num);
