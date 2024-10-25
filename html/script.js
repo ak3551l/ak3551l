f929ff817b48a75449a6ad9bf4b4c23d4b287f5c
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-10-25 16:15:28 +0000

    Fri, Oct 25, 2024, 9:45 PM +05:30

--- html/script.js
+++ html/script.js
@@ -1,1 +1,7 @@
-const aboutMeLatest = '
+const person = {
+    name: 'Abhijeet',
+    age: 20
+}
+
+const aboutMeLatest = `My name is ${person.name} and I am ${person.age} years old`
+console.log(aboutMeLatest);
