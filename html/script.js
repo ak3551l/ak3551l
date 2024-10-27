06742dba03bd32e23697758110599989d551fff7
ak3551l <104051639+ak3551l@users.noreply.github.com>
2024-10-27 03:54:12 +0000

    Sun, Oct 27, 2024, 9:24 AM +05:30

--- html/script.js
+++ html/script.js
@@ -1,1 +1,11 @@
+const promiseObject = fetch('/data.json');
+
+promiseObject.then(response => {
+    const promiseObject2 = response.json();
+    promiseObject2.then(data => {
+        console.log(data);
+    });
+}).catch(error => {
+    console.error('Error fetching data:', error);
+});
 
